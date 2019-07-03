ab80d5e1ea0b3aeb0c6fa68afaeaba6b9a77b497
James Hotblack <james.c.hotblack@gmail.com>
2019-07-03 16:26:21 +0100

    Made main.rb output more user friendly

--- main.rb
+++ main.rb
@@ -4,7 +4,10 @@ require_relative 'lib/parser'
 
 parser = Parser.new(ARGV[0])
 parser.parse
+puts "\n"
 puts 'Total Visits'
 parser.ordered_views
+puts "\n"
 puts 'Total Unique Visits'
 parser.ordered_unique_views
+puts "\n"
